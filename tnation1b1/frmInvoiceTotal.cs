749912dea0944c306ac7ddf5295f50eeeda7c1fd
Tyler Nation <wn4926xl@go.minnstate.edu>
2018-08-31 19:26:42 -0500

    Created Ex1B

---
+++ tnation1b1/frmInvoiceTotal.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace tnation1b1
+{
+    public partial class frmInvoiceTotal : Form
+    {
+        public frmInvoiceTotal()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //txtTotal.Text = "10";
+            //txtTotal.ReadOnly = true;
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
