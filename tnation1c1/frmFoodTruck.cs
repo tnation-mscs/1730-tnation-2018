1c6e1d043ff49c6b2b08aced6ad16d2efc99c01e
Tyler Nation <wn4926xl@go.minnstate.edu>
2018-09-11 16:23:41 -0500

    Completed ex1e

--- tnation1c1/frmFoodTruck.cs
+++ tnation1c1/frmFoodTruck.cs
@@ -56,5 +56,6 @@ namespace tnation1c1
 
             txtHotDogs.Focus();
         }
+
     }
 }
---
+++ tnation1e1/frmAverageScore.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace tnation1e1
+{
+    public partial class tnation1e1 : Form
+    {
+        public tnation1e1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            txtAverage.Text = ((
+                Convert.ToDecimal(txtTest1.Text)
+                + Convert.ToDecimal(txtTest2.Text)
+                + Convert.ToDecimal(txtTest3.Text))
+                / 3
+                ).ToString("0.00");
+
+            btnClear.Focus();
+
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            txtTest1.Text = "0";
+            txtTest2.Text = "0";
+            txtTest3.Text = "0";
+            txtAverage.Text = "";
+
+            txtTest1.Focus();
+
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
